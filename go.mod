ee0eafe2e825c10944f0ed6fa1a77977b031774c
RzaAhmadi <barzin62@gmail.com>
2024-04-17 03:33:10 -0700

    Initial commit

---
+++ go.mod
@@ -0,0 +1,6 @@
+module github.com/rezaahmadi/prj
+
+go 1.21.6
+
+require github.com/go-chi/chi v1.5.5 
+// indirect
---
+++ pkg/handlers/handlers.go
@@ -0,0 +1,34 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/rezaahmadi/prj/pkg/config"
+	"github.com/rezaahmadi/prj/pkg/models"
+	"github.com/rezaahmadi/prj/pkg/render"
+)
+
+// Repo the repository used by the handlers
+var Repo *Repository
+
+// Repository is the repository type
+type Repository struct {
+	App *config.AppConfig
+}
+
+// NewRepo creates a new repository
+func NewRepo(a *config.AppConfig) *Repository {
+	return &Repository{
+		App: a,
+	}
+}
+
+// NewHandlers sets the repository for the handlers
+func NewHandlers(r *Repository) {
+	Repo = r
+}
+
+// Home is the handler for the home page
+func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "home.page.tmpl", &models.TemplateData{})
+}
